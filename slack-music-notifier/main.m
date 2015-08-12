b2837469c68840ea79a370d0789c8a9fff3bdbbb
Chris Ellsworth <cellsworth@gmail.com>
2015-08-11 22:26:04 -0400

    Initial Commit

---
+++ slack-music-notifier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  slack-music-notifier
+//
+//  Created by Chris Ellsworth on 8/11/15.
+//  Copyright (c) 2015 Chris Ellsworth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
